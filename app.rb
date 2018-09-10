8fbd3483337c0b70cdff5a4e3d637b690028428c
Mary Jack <maryjackinthebox@gmail.com>
2018-09-10 21:50:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
-
-class App < Sinatra::Base
-end
+ class App < Sinatra::Base	
+   configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+   get '/' do 
+    @session = session
+    erb :index
+  end
+   post '/checkout' do
+  session[:item] = params[:item]
+  @session = session
+  erb :checkout
+  end
+end 	
