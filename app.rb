f199586984d52adcb7752921dcdace5423f8a9c0
Mary Jack <maryjackinthebox@gmail.com>
2018-09-10 21:50:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,9 +1,8 @@
 require_relative 'config/environment'
-
- class App < Sinatra::Base	class App < Sinatra::Base
+ class App < Sinatra::Base	
    configure do
     enable :sessions
-    set :session_secret, "shakshuka"
+    set :session_secret, "secret"
   end
    get '/' do 
     @session = session
@@ -14,4 +13,4 @@ require_relative 'config/environment'
   @session = session
   erb :checkout
   end
-end 
+end 	
